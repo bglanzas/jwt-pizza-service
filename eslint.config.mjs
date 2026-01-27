c15e98ce302350da456808ce5155a441a7290845
bglanzas <bglanzas@gmail.com>
2026-01-27 16:21:46 -0700

    fixing lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,13 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.node, ...globals.browser } },
+  },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.test.js"], languageOptions: { globals: { ...globals.jest } } },
+  { files: ["**/*.test.js"], languageOptions: { globals: { ...globals.jest } } }
 ]);
