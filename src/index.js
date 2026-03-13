2d48856ad186f39f32798fe5ba6180867d124349
bglanzas <bglanzas@gmail.com>
2026-03-13 11:33:07 -0600

    more aws stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const app = require('./service.js');
-
+console.log("ROOT INDEX STARTED");
+const metrics = require('./metrics');
+console.log("ABOUT TO START METRICS");
+metrics.startReporting();
 const port = process.argv[2] || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
