fa17586002f89c673289e611f6bbefd385448269
bglanzas <bglanzas@gmail.com>
2026-03-13 12:08:07 -0600

    this is a test

--- src/database/database.js
+++ src/database/database.js
@@ -374,7 +374,7 @@ class DB {
       try {
         const dbExists = await this.checkDatabaseExists(connection);
         console.log(dbExists ? 'Database exists' : 'Database does not exist, creating it');
-
+        console.log('test')
         await connection.query(`CREATE DATABASE IF NOT EXISTS ${config.db.connection.database}`);
         await connection.query(`USE ${config.db.connection.database}`);
 
